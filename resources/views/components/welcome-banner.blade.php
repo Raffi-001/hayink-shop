094ec3d1ad26938f4fb5b911b5e7e22730e58bc6
Raffi <hovhannesian@gmail.com>
2025-09-19 17:40:35 +0400

    text change

--- resources/views/components/welcome-banner.blade.php
+++ resources/views/components/welcome-banner.blade.php
@@ -22,7 +22,7 @@
         <img src="images/hero1.png" alt="" class="absolute inset-0 h-full w-full object-cover object-center">
         <div class="relative flex w-full flex-col items-start justify-center bg-black bg-opacity-40 p-8 sm:p-12">
             <h2 class="text-lg font-medium text-white text-opacity-75">Cozy Up</h2>
-            <p class="mt-1 text-5xl font-medium text-white max-w-5xl font-accent">Stay stylish with our range of t-shirts</p>
+            <p class="mt-1 text-5xl font-medium text-white max-w-5xl font-accent">The first platform built for Armenian artists to rise beyond borders.</p>
             <a href="{{ route('products.index') }}" class="mt-4 rounded-md bg-white px-4 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-50">Shop now</a>
         </div>
     </div>
